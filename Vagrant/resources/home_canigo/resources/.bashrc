9f6fcbedc51dc9d4f7c5240601b1b8a6673c142a
Y7838845E <juan.pablo.jimenez@ticxcat.cat>
2022-01-21 09:13:01 +0100

    Change base OS version and general refactoring

--- Vagrant/resources/home_canigo/resources/.bashrc
+++ Vagrant/resources/home_canigo/resources/.bashrc
@@ -74,14 +74,11 @@ esac
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
 
 # colored GCC warnings and errors
@@ -98,7 +95,7 @@ alias l='ls -CF'
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
 
 if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
+  . ~/.bash_aliases
 fi
 
 # Add an "alert" alias for long running commands.  Use like so:
@@ -107,7 +104,7 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 
 
 if [ -f ~/.bash_functions ]; then
-    . ~/.bash_functions
+  . ~/.bash_functions
 fi
 
 # enable programmable completion features (you don't need to enable
@@ -121,6 +118,5 @@ if ! shopt -oq posix; then
   fi
 fi
 
-# setxkbmap -layout 'es,es' -model pc105
-java8
-
+java11
+node16
--- Vagrant/resources/provision-common.sh
+++ Vagrant/resources/provision-common.sh
@@ -1,114 +1,100 @@
-### sourced
+#!/usr/bin/env bash
 
-log () {
-    echo "$(date "+%d-%m-%Y %H:%M:%S") $0 [provision] $*"
+function log () {
+  echo "$(date "+%d-%m-%Y %H:%M:%S") ${0} [provision] ${*}"
 }
 
-die () {
-    log "[ERROR] $*. Exiting..."
-    exit $1
+function die () {
+  log "[ERROR] ${*}. Exiting..."
+  exit "${1}"
 }
 
-_apt_get () {
-    apt-get -q -y $*
+function _apt_get () {
+  apt-get -q -y $@
 }
 
-_wget () {
-    wget -q -O $*
+function _wget () {
+  wget -q -O "$@"
 }
 
-_prepare_and_reboot () {
+function _prepare_and_reboot () {
+  log "Preparant següent fase (${1}) ..."
 
-    log "Preparant següent fase ($1) ..."
-
-cat > /provision.sh.crontab<<EOF
-@reboot /provision.sh $1 | tee /dev/tty0 | tee -a /provision.sh.log
+  cat > /provision.sh.crontab<<EOF
+@reboot /provision.sh ${1} | tee /dev/tty0 | tee -a /provision.sh.log
 EOF
 
-    crontab /provision.sh.crontab
+  crontab /provision.sh.crontab
 
-    log 'Rebooting...'
+  log 'Rebooting...'
 
-    # Petita pausa per poder veure els missatges
-    sleep $REBOOT_SLEEP
+  # Petita pausa per poder veure els missatges
+  sleep "${REBOOT_SLEEP:-10}"
 
-    reboot
+  reboot
 }
 
-do_install () {
+function _decompress () {
+  [ -f "${1}" ] || die 1 "Fitxer ${1} no trobat"
 
-    _BN=$(basename $1)
+  if [ "$( echo "${1}" | grep -E -q '\.tar\.gz$' ; echo $? )" -eq 0 ] ; then
+    tar -xzvf "${1}"
+  elif [ "$( echo "${1}" | grep -E -q '\.tar\.xz$' ; echo $? )" -eq 0 ] ; then
+    tar -xJvf "${1}"
+  elif [ "$( echo "${1}" | grep -E -q '\.tgz$' ; echo $? )" -eq 0 ] ; then
+    tar -xzvf "${1}"
+  elif [ "$( echo "${1}" | grep -E -q '\.zip$' ; echo $? )" -eq 0 ] ; then
+    unzip "${1}"
+  elif [ "$( echo "${1}" | grep -E -q '\.xz$' ; echo $? )" -eq 0 ] ; then
+    unxz "${1}"
+  elif [ "$( echo "${1}" | grep -E -q '\.bz$' ; echo $? )" -eq 0 ] ; then
+    bzip2 -d "${1}"
+  elif [ "$( echo "${1}" | grep -E -q '\.deb$' ; echo $? )" -eq 0 ] ; then
+    apt install -y "${1}"
+  else
+    die 4 "Fitxer ${1} no reconegut"
+  fi
+}
 
-    log "Instal.lant $_BN ..."
+function do_install () {
+  local -r _BN=$(basename "${1}")
 
-    cd /opt
+  log "Instal.lant ${_BN} ..."
 
-    _decompress () {
+  cd /opt || exit
 
-        [ -f $1 ] || die 1 "Fitxer $1 no trobat"
+  if [ -f "${_BN}" ]; then
+    log "S'utilitza el fitxer $_BN present prèviament"
+    _decompress "${_BN}" || die 2
+  else
+    wget -nv "${1}" || die 1
+    _decompress "${_BN}" || die 3
+    rm "${_BN}"
+  fi
 
-        if [ $( echo $1 | egrep -q '\.tar\.gz$' ; echo $? ) -eq 0 ] ; then
-            tar -xzvf $1
-        elif [ $( echo $1 | egrep -q '\.tar\.xz$' ; echo $? ) -eq 0 ] ; then
-            tar -xJvf $1
-        elif [ $( echo $1 | egrep -q '\.tgz$' ; echo $? ) -eq 0 ] ; then
-            tar -xzvf $1
-        elif [ $( echo $1 | egrep -q '\.zip$' ; echo $? ) -eq 0 ] ; then
-            unzip $1
-        elif [ $( echo $1 | egrep -q '\.xz$' ; echo $? ) -eq 0 ] ; then
-            unxz $1
-        elif [ $( echo $1 | egrep -q '\.bz$' ; echo $? ) -eq 0 ] ; then
-            bzip2 -d $1
-        elif [ $( echo $1 | egrep -q '\.deb$' ; echo $? ) -eq 0 ] ; then
-            apt install -y $1
-        else
-            die 4 "Fitxer $1 no reconegut"
-        fi
-
-    }
-
-    if [ -f $_BN ]; then
-
-        log "S'utilitza el fitxer $_BN present prèviament"
-
-        _decompress $_BN || die 2
-
-    else
-        wget -nv $1 || die 1
-
-        _decompress $_BN || die 3
-
-        rm $_BN
-    fi
-
-    log "Instal.lat $_BN"
+  log "Instal.lat ${_BN}"
 }
 
 # Fa un "page scrapping" d'una pàgina (normalment una pàgina de descàrrega) i descarrega un arxiu per a la seva instal·lació.
-#
-# @param 1
-#       URL on cercar l'enllaç de descàrrega
-# @param 2
-#       REGEXP que ha de tenir l'enllaç de descàrrega
-do_install2 () {
+# @param 1: URL on cercar l'enllaç de descàrrega
+# @param 2: REGEXP que ha de tenir l'enllaç de descàrrega
+function do_install2 () {
+  log "Accedint a ${1} ..."
 
-    log "Accedint a $1 ..."
+  local -r __TMP=/tmp/do_install2-$$.html.tmp
 
-    local __TMP=/tmp/do_install2-$$.html.tmp
+  lynx -dump "${1}" > "${__TMP}" || die 1
 
-    lynx -dump $1 > $__TMP || die 1
+  _URL=$(grep -E -o "${2}" $__TMP | grep -E '\.(zip|gz|tgz|xz|bz|bz2)$' | head -1)
+  [ -z "${_URL}" ] && die 2
 
-    _URL=$(egrep -o $2 $__TMP | egrep '\.(zip|gz|tgz|xz|bz|bz2)$' | head -1)
-    [ -z $_URL ] && die 2
-
-    do_install $_URL
+  do_install "${_URL}"
 }
 
 wait4jobs () {
-
-    while true; do
-        jobs >/dev/null
-        jobs -r | wc -l | grep -q ^0 && break
-        sleep 3
-    done
+  while true; do
+    jobs >/dev/null
+    jobs -r | wc -l | grep -q ^0 && break
+    sleep 3
+  done
 }
